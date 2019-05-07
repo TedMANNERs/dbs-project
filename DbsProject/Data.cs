1d05cc0f7aa8cba508c192b655ad00850a96bf8b
TedMANNERs <mannersofted@gmail.com>
2019-05-07 11:10:09 +0200

    Add data class, try reading keys

---
+++ DbsProject/Data.cs
@@ -0,0 +1,17 @@
+﻿namespace DbsProject
+{
+    internal class Data
+    {
+        public string BfsNr { get; set; }
+        public string GebietName { get; set; }
+        public string ThemaName { get; set; }
+        public string SetName { get; set; }
+        public string SubsetName { get; set; }
+        public string IndikatorId { get; set; }
+        public string IndikatorName { get; set; }
+        public string IndikatorJahr { get; set; }
+        public string IndikatorValue { get; set; }
+        public string EinheitKurz { get; set; }
+        public string EinheitLang { get; set; }
+    }
+}
---
+++ DbsProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace DbsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<string> values = new List<string>();
+
+            Console.WriteLine("Begin import");
+            using (var muxer = ConnectionMultiplexer.Connect("localhost"))
+            {
+                var server = muxer.GetServer("localhost", 6379);
+                var db = muxer.GetDatabase();
+
+                foreach (var key in server.Keys(pattern: "*"))
+                {
+                    Console.WriteLine(key);
+                    var value = db.StringGet(key);
+                    values.Add(value);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
